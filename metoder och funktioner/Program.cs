f56ce028872237c189aa12d0ec21d77024a4c3c3
Emil Stener <stener99@hotmail.se>
2016-11-11 14:20:18 +0100

    Add project files.

---
+++ metoder och funktioner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace metoder_och_funktioner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
