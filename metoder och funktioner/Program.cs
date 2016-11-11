e1ec9b887b322faa4f923333588cd4ffaf3bc816
Emil Stener <stener99@hotmail.se>
2016-11-11 15:11:48 +0100

    asdasdasd

--- metoder och funktioner/Program.cs
+++ metoder och funktioner/Program.cs
@@ -8,8 +8,52 @@ namespace metoder_och_funktioner
 {
     class Program
     {
+        //övning1 
+        /*
         static void Main(string[] args)
         {
+            
+            double temp = int.Parse(Console.ReadLine());
+            Console.WriteLine(CelsiusTillFarenheit(temp) + " grader i Farenheit");
+            Console.ReadKey();
+            
         }
+        static double CelsiusTillFarenheit(double temp)
+        {
+            return (temp / 5.0) * 9 + 32;
+        }
+        
+
+
+        //övning2
+        static void Main(string[] args)
+        {
+
+            double radie = int.Parse(Console.ReadLine());
+            Console.WriteLine(Arie(radie) + " aria");
+            Console.WriteLine(omkrets(radie) + " omkrets");
+            Console.ReadKey();
+
+        }
+        static double Arie(double radie)
+        {
+            return (radie * radie * Math.PI);
+        }
+        static double omkrets(double radie)
+        {
+            return (radie * 2* Math.PI);
+
+        }*/
+        //övning3
+        static void Main(string[] args)
+        {
+
+            double radie = int.Parse(Console.ReadLine());
+            Console.WriteLine(() + " antal tärningar ");
+            Console.WriteLine(() + "resultat");
+            Console.ReadKey();
+
+        }
+
     }
 }
