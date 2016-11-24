72afddb7a748ba6ca85819dd7dbb203e393ce7f5
Emil Stener <stener99@hotmail.se>
2016-11-24 19:09:28 +0100

    asd

--- metoder och funktioner/Program.cs
+++ metoder och funktioner/Program.cs
@@ -43,17 +43,71 @@ namespace metoder_och_funktioner
         {
             return (radie * 2* Math.PI);
 
-        }*/
+        }
+        
         //övning3
         static void Main(string[] args)
         {
 
-            double radie = int.Parse(Console.ReadLine());
-            Console.WriteLine(() + " antal tärningar ");
-            Console.WriteLine(() + "resultat");
+            Console.Write("Antal tärnings kast: ");
+            int antalKast = int.Parse(Console.ReadLine());
+            Console.Write("värde: ");
+            Console.WriteLine (Tärning(antalKast));
             Console.ReadKey();
+        }
+        static int Tärning(int antalKast)
+        {
+            Random rnd = new Random();
+            int resultat;
+            int sum = 0;
+            for (int i = 0; i < antalKast; i++)
+            {
+                resultat = rnd.Next(1, 7);
+                sum += resultat;
+            }
+            return sum;
+
+
 
         }
+    }
+            
+            
+        //övning 4
+        static void Main(string[] args)
+        {
+            Console.Write("skriv in antal sidor på tärning: ");
+            int antalSidor = int.Parse(Console.ReadLine());
+            Console.Write("skriv in antall kast: ");
+            int antalKast = int.Parse(Console.ReadLine());
+            Tärning(antalSidor, antalKast);
+            Console.WriteLine("Resultat: " + Tärning(antalSidor, antalKast));
+            Console.ReadKey();
+        }
+          static int Tärning(int kast)
+        {
+            Random rnd = new Random();
+            int resultat;
+            int sum = 0;
+            for (int i = 0; i < kast; i++)
+            {
+                resultat = rnd.Next(1, 7);
+                sum += resultat;
+            }
+            return sum;
+        }
+        static int Tärning(int antalSidor, int antalKast)
+        {
+            Random rnd = new Random();
+            int total = 0;
+            for (int i = 0; i < antalKast; i++)
+                total += rnd.Next(1, antalSidor + 1);
+            return total;
+            }
+    */
 
     }
-}
+
+    }
+    
+
